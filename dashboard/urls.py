6d7fd616d607dd8ff8a5632903b4c936d316b787
Rohit-24b3002 <24b3002@iitb.ac.in>
2025-09-07 14:52:46 +0530

    changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('student/', views.student_dashboard, name='student_dashboard'),
     path('teacher/', views.teacher_dashboard, name='teacher_dashboard'),
+    path("logout/", views.logout_view, name="logout"),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 @login_required
 def student_dashboard(request):
@@ -8,3 +9,7 @@ def student_dashboard(request):
 @login_required
 def teacher_dashboard(request):
     return render(request, 'dashboard/teacher_dashboard.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
